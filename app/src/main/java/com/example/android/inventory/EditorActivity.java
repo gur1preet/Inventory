761f0f0c1fd3c660262f113a17a2d75be0c80d24
gur1preet <gurpreet.pahul@hotmail.com>
2017-03-03 22:16:05 +0530

    layout

---
+++ app/src/main/java/com/example/android/inventory/EditorActivity.java
@@ -0,0 +1,45 @@
+package com.example.android.inventory;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class EditorActivity extends AppCompatActivity {
+
+    private EditText mNameEditText = (EditText)findViewById(R.id.edit_name);
+    private EditText mDescriptionEditText = (EditText)findViewById(R.id.edit_description);
+    private EditText mAvailableQuantityEditText = (EditText)findViewById(R.id.edit_quantity);
+    private EditText mOrderedQuantityEditText = (EditText)findViewById(R.id.edit_available_quantity);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                return true;
+            case R.id.action_delete:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
